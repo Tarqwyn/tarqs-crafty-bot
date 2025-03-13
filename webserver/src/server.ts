0334ccd39581d776094082888f8a18a7b6be00b2
root <root@Tarqwyn-Media.>
2025-03-13 13:53:18 +0000

    Simple webserver: Compile LOCALLY

--- webserver/src/server.ts
+++ webserver/src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = 80;
 
 app.get('/', (req, res) => {
   res.send('Tarqs Crafty Bot is running');
